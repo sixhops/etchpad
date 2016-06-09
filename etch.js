fdc6799fcf6709a815b9ca1de4fb47ee5f0656a9
Steve Peters <sixhops@gmail.com>
2016-06-09 13:03:59 -0700

    Basic drawing functionality checkin

--- etch.js
+++ etch.js
@@ -1,3 +1,29 @@
+
+var makeDivs = function(length) {
+	var $myDiv;
+	for (var i = 0; i < length; i++) {
+		for (var j = 0; j < length; j++) {
+			$myDiv = $("<div class='cell'></div>");
+			$("#container").append($myDiv);
+		}
+	}
+	$(".cell").css("width", (640 / length));
+	$(".cell").css("height", (640 / length));
+	$(".cell").css("background-color", "#ccc");
+	$(".cell").css("margin", 0);
+	$(".cell").css("padding", 0);
+	$(".cell").css("float", "left");
+};
+
+
 $(document).ready(function() {
-    
+    $("#container").css("background-color", "#ccc");
+
+    var length = prompt("Please enter a length for the sides.");
+
+    makeDivs(length);
+
+    $(".cell").mouseenter(function() {
+    	$(this).css("background-color", "#000");
+    });
 });
